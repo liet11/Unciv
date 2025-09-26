126a79fb1f1bbd92f136fc926d1f477b28180594
liet11 <126644064+liet11@users.noreply.github.com>
2025-09-26 10:33:59 +0900

    Update Constants.kt

--- core/src/com/unciv/Constants.kt
+++ core/src/com/unciv/Constants.kt
@@ -27,6 +27,8 @@ object Constants {
     const val grassland = "Grassland"
     const val tundra = "Tundra"
     const val snow = "Snow"
+    const val shelling = "Shelling"
+    const val trench = "Trench"
 
     const val forest = "Forest"
     const val jungle = "Jungle"
